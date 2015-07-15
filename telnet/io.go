ef00cb54da15d144f69a947fa6acacbfe9f8a260
Amrit Panesar <amrit@station-app.com>
2015-07-15 13:38:18 -0700

    restructure
    Signed-off-by: Amrit Panesar <amrit@station-app.com>

--- telnet/io.go
+++ telnet/io.go
@@ -13,6 +13,12 @@ func main() {
 	fileh, _ := os.Open(filename)
 	scanner := bufio.NewScanner(fileh)
 	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) >= 1 {
+			if line[0] > '0' && line[0] <= '9' {
+				fmt.Println("found a new panel, wait time", line)
+			}
+		}
 		fmt.Println(scanner.Text())
 	}
 	if err := scanner.Err(); err != nil {
