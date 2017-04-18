d6c04cbf6db32e74f82cb6a058e62c6f42930ebc
Amrit Panesar <apanesar@paylease.com>
2017-04-18 10:55:14 -0700

    Quick refactor, havent looked at this code in a while

--- telnet/main.go
+++ telnet/main.go
@@ -11,9 +11,7 @@ import (
 )
 
 // http://semver.org/
-const version = "0.1.0"
-
-var globalFrames []FrameGroup
+const version = "0.2.0"
 
 // Defines a single Frame
 type Frame struct {
@@ -21,31 +19,17 @@ type Frame struct {
 	Frame   string
 }
 
-type FrameGroup struct {
-	V4Frame []Frame
-	V6Frame []Frame
-}
-
-// Do some basic logging for any error
-func checkErr(err error) bool {
+// Load a specified file into the Frames slice
+func parseFile(fileName string, stringOut chan<- Frame) {
+	fileh, err := os.Open(fileName)
 	if err != nil {
 		log.Fatalln("Error encountered:", err)
-		return true
-	}
-	return false
-}
-
-// Load a specified file into the Frames slice
-func loadFile(fileName string, stringOut chan<- Frame) {
-	fileh, err := os.Open(fileName)
-
-	if checkErr(err) {
 		return
 	}
 
 	scanner := bufio.NewScanner(fileh)
 
-	thisFrame := Frame{
+	nextFrame := Frame{
 		Timeout: 0,
 		Frame:   "START",
 	}
@@ -55,34 +39,34 @@ func loadFile(fileName string, stringOut chan<- Frame) {
 
 		if len(line) >= 1 {
 			if line[0] > '0' && line[0] <= '9' {
-				if thisFrame.Frame != "START" {
-					stringOut <- thisFrame
+				if nextFrame.Frame != "START" {
+					stringOut <- nextFrame
 				}
-				thisFrame = Frame{}
+				nextFrame = Frame{}
 				uhold, _ := strconv.ParseInt(line, 10, 64)
-				thisFrame.Timeout = uhold
+				nextFrame.Timeout = uhold
 			} else {
-				thisFrame.Frame = fmt.Sprintln(thisFrame.Frame, line, "\r")
+				nextFrame.Frame = fmt.Sprintln(nextFrame.Frame, line, "\r")
 			}
 		} else {
-			thisFrame.Frame = fmt.Sprintln(thisFrame.Frame, line, "\r")
+			nextFrame.Frame = fmt.Sprintln(nextFrame.Frame, line, "\r")
 		}
 
 	}
 
-	stringOut <- thisFrame
+	stringOut <- nextFrame
 
 	close(stringOut)
 
 }
 
 // Generates a slice of Frames from a filename
-// calls loadFile()
-func load(fileName string) []Frame {
+// calls parseFile()
+func loadFile(fileName string) []Frame {
 	log.Println("Loading", fileName)
 	Frames := make(chan Frame, 100)
 
-	go loadFile(fileName, Frames)
+	go parseFile(fileName, Frames)
 
 	outFrame := make([]Frame, 0)
 
@@ -95,6 +79,7 @@ func load(fileName string) []Frame {
 
 }
 
+// Sends frames over the wire at 15 fps
 func PlayFrame(con net.Conn, frames []Frame) {
 	for _, frame := range frames {
 		fmt.Fprintf(con, "\x1b[2J\x1b[H%s", frame.Frame)
@@ -105,20 +90,20 @@ func PlayFrame(con net.Conn, frames []Frame) {
 func main() {
 	fmt.Println("Go ASCII Service version", version)
 
-	globalFrames = make([]FrameGroup, 0)
+	v4Frames := make([]Frame, 0)
+	v6Frames := make([]Frame, 0)
 
-	sw1 := FrameGroup{}
 	sem := make(chan string, 2)
 
 	go func(fileName string) {
 		log.Println("File:", fileName, "has begun loading")
-		sw1.V4Frame = load(fileName)
+		v4Frames = loadFile(fileName)
 		sem <- fileName
 	}("swv4.txt")
 
 	go func(fileName string) {
 		log.Println("File:", fileName, "has begun loading")
-		sw1.V6Frame = load(fileName)
+		v6Frames = loadFile(fileName)
 		sem <- fileName
 	}("swv6.txt")
 
@@ -127,11 +112,10 @@ func main() {
 
 	close(sem)
 
-	globalFrames = append(globalFrames, sw1)
+	sock, err := net.Listen("tcp", ":2323")
 
-	sock, err := net.Listen("tcp", ":23")
-
-	if checkErr(err) {
+	if err != nil {
+		log.Fatalln("Error encountered:", err)
 		return
 	}
 
@@ -139,17 +123,23 @@ func main() {
 
 	for {
 		conn, err := sock.Accept()
-		if checkErr(err) {
-			return
+		if err != nil {
+			log.Fatalln("Error encountered:", err)
+			break
 		}
+
 		test, err := net.ResolveTCPAddr("tcp", sock.Addr().String())
+		if err != nil {
+			log.Fatalln("Error encountered:", err)
+			break
+		}
 
 		if test.IP.To4() != nil {
 			log.Println("New v4 User: ", sock.Addr().String())
-			go PlayFrame(conn, globalFrames[0].V4Frame)
+			go PlayFrame(conn, v4Frames)
 		} else {
 			log.Println("New v6 User: ", sock.Addr().String())
-			go PlayFrame(conn, globalFrames[0].V6Frame)
+			go PlayFrame(conn, v6Frames)
 		}
 	}
 }
